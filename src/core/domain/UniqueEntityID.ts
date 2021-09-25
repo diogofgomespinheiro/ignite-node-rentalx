6b959a642941f349a7f5105c167c6d9c18a9dd96
Diogo Pinheiro <diogo.fgomes.pinheiro@gmail.com>
2021-09-26 00:29:17 +0100

    📦 refactor: replace nanoid for uuid

--- src/core/domain/UniqueEntityID.ts
+++ src/core/domain/UniqueEntityID.ts
@@ -1,9 +1,9 @@
-import { nanoid } from 'nanoid';
+import { v4 as uuidv4 } from 'uuid';
 
 import { Identifier } from './Identifier';
 
 export class UniqueEntityID extends Identifier<string> {
   constructor(id?: string) {
-    super(id || nanoid());
+    super(id || uuidv4());
   }
 }
